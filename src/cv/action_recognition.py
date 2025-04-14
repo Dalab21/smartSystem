715e3d19c3c8164a54afa8b88f82ee97722cace4
SeydouDAOU <daouseydou@yahoo.com>
2025-04-14 11:48:32 +0200

    update file action_recognition

--- src/cv/action_recognition.py
+++ src/cv/action_recognition.py
@@ -1,13 +1,15 @@
 import torch
 import numpy as np
-from torchvision.models.video import r3d_18  # Modèle 3D CNN de PyTorch
+# model 3D CNN (PyTorch)
+from torchvision.models.video import r3d_18  
 from torchvision.transforms import Compose, Resize, CenterCrop, ToTensor
 
 class ActionRecognizer:
     def __init__(self):
         self.model = r3d_18(pretrained=True)
         self.model.eval()
-        self.labels = ["Marcher", "Courir", "Sauter", "Pousser", "Tomber"]  # Exemple
+        # exples de mouvements
+        self.labels = ["Marcher", "Courir", "Sauter", "Pousser", "Tomber"]  
         self.transform = Compose([
             Resize((112, 112)),
             CenterCrop((112, 112)),
@@ -16,14 +18,14 @@ class ActionRecognizer:
 
     def predict(self, video_frames):
         """Input: Liste de frames (np.array)"""
-        # Préprocessing
+        # Preprocessing
         frames = [self.transform(Image.fromarray(frame)) for frame in video_frames]
-        clip = torch.stack(frames).unsqueeze(0)  # Shape: [1, T, C, H, W]
+        clip = torch.stack(frames).unsqueeze(0)  # shape: [1, T, C, H, W]
         
-        # Inference
+        # inference
         with torch.no_grad():
             outputs = self.model(clip)
         
-        # Post-processing
+        # post-processing
         pred = torch.argmax(outputs).item()
         return self.labels[pred]
